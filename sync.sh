0a789a3ea0bd13bcef23fd4fe0df36cf931e01fc
phil <phil@branch14.org>
2017-03-03 19:13:40 +0100

    improve sync script

--- sync.sh
+++ sync.sh
@@ -2,23 +2,19 @@
 
 prefix=s3://www.bos-schweiz.ch
 
-# 1. create a temp file to hold html
+# create a temp file to hold html
 tmp=$(mktemp --suffix=.html)
 
-# 2. retrieve html from primary hosting into temp file
-curl -o $tmp https://b2bw.github.io/com-dev/
+# retrieve html from primary hosting into temp file
+wget -kO $tmp https://b2bw.github.io/com-dev/
 
-# 3. upload temp file to secondary hosting via s3
+# upload temp file to secondary hosting via s3
 aws s3 cp --acl public-read $tmp \
     $prefix/de/projekte/entwicklungszusammenarbeit.htm
 
-# 4. remove temp file
+# remove temp file
 rm $tmp
 
-
-
-
-
-# 5. send an email to local user root
+# send an email to local user root
 echo "http://www.bos-schweiz.ch/de/projekte/entwicklungszusammenarbeit.htm" \
     | mail -s "ComDev has been updated." root
