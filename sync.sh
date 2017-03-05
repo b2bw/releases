90f45e19e461672d479a082957af2dacbdb8e94c
phil <phil@branch14.org>
2017-03-06 00:03:15 +0100

    update sync

--- sync.sh
+++ sync.sh
@@ -6,15 +6,15 @@ prefix=s3://www.bos-schweiz.ch
 tmp=$(mktemp --suffix=.html)
 
 # retrieve html from primary hosting into temp file
-wget -kO $tmp https://b2bw.github.io/com-dev/
+wget -kO $tmp https://b2bw.github.io/releases/
 
 # upload temp file to secondary hosting via s3
 aws s3 cp --acl public-read $tmp \
-    $prefix/de/projekte/entwicklungszusammenarbeit.htm
+    $prefix/de/orang-utan-auswilderung.htm
 
 # remove temp file
 rm $tmp
 
 # send an email to local user root
-echo "http://www.bos-schweiz.ch/de/projekte/entwicklungszusammenarbeit.htm" \
-    | mail -s "ComDev has been updated." root
+echo "http://www.bos-schweiz.ch/de/orang-utan-auswilderung.htm" \
+    | mail -s "LP Releases has been updated." root
