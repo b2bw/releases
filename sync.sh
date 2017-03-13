9a93a74be2c67f473411fec4b6349e78087f178b
Nick Niles <nickyniles@gmail.com>
2017-03-13 13:43:28 +0100

    fix sync

--- sync.sh
+++ sync.sh
@@ -5,12 +5,47 @@ prefix=s3://www.bos-schweiz.ch
 # create a temp file to hold html
 tmp=$(mktemp --suffix=.html)
 
+# MAPPING
+# -------
+# http://born2bewild.org/releases/                        -> /de/orang-utan-auswilderung.htm
+# http://born2bewild.org/releases/ost-kalimantan.html     -> /de/orang-utan-auswilderung/ost-kalimantan.htm
+# http://born2bewild.org/releases/salat-island.html       -> /de/orang-utan-auswilderung/salat-island.htm
+# http://born2bewild.org/releases/technische-details.html -> /de/orang-utan-auswilderung/technische-details.htm
+
+base='http://born2bewild.org/releases'
+target='/de/orang-utan-auswilderung'
+
 # retrieve html from primary hosting into temp file
-wget -kO $tmp https://b2bw.github.io/releases/
+wget -kO $tmp $base/
+
+# convert specific links
+sed -i "s|$base/ost-kalimantan.html|$target/ost-kalimantan.htm|" $tmp
+sed -i "s|$base/salat-island.html|$target/salat-island.htm|" $tmp
+sed -i "s|$base/technische-details.html|$target/technische-details.htm|" $tmp
 
 # upload temp file to secondary hosting via s3
-aws s3 cp --acl public-read $tmp \
-    $prefix/de/orang-utan-auswilderung.htm
+aws s3 cp --acl public-read $tmp $prefix$target.htm
+
+# ost-kalimantan
+wget -kO $tmp $base/ost-kalimantan.html
+sed -i "s|$base/salat-island.html|$target/salat-island.htm|" $tmp
+sed -i "s|$base/technische-details.html|$target/technische-details.htm|" $tmp
+sed -i "s|$base/#ost-kalimantan|$target.htm#ost-kalimantan|" $tmp
+aws s3 cp --acl public-read $tmp $prefix$target/ost-kalimantan.htm
+
+# salat-island
+wget -kO $tmp $base/salat-island.html
+sed -i "s|$base/ost-kalimantan.html|$target/ost-kalimantan.htm|" $tmp
+sed -i "s|$base/technische-details.html|$target/technische-details.htm|" $tmp
+sed -i "s|$base/#salat-island|$target.htm#salat-island|" $tmp
+aws s3 cp --acl public-read $tmp $prefix$target/salat-island.htm
+
+# technische-details
+wget -kO $tmp $base/technische-details.html
+sed -i "s|$base/ost-kalimantan.html|$target/ost-kalimantan.htm|" $tmp
+sed -i "s|$base/salat-island.html|$target/salat-island.htm|" $tmp
+sed -i "s|$base/#technische-details|$target.htm#technische-details|" $tmp
+aws s3 cp --acl public-read $tmp $prefix$target/technische-details.htm
 
 # remove temp file
 rm $tmp
